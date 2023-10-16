97b5f73cab44f814a02de9dd830081f82b578692
xmartina <martinsprince126@gmail.com>
2023-10-16 16:12:01 +0100

    fixed

--- application/views/backend/users/view/debit.php
+++ application/views/backend/users/view/debit.php
@@ -1,1 +1,12 @@
 <?php
+$targetId =  $this->security->xss_clean($userInfo->userId);
+$ppic = $userInfo->ppic == '' ? base_url('assets/dist/img/avatar.png') : $ppic;
+$fname = $this->security->xss_clean($userInfo->firstName);
+$lname =  $this->security->xss_clean($userInfo->lastName);
+$email =  $this->security->xss_clean($userInfo->email);
+$mobile =  $this->security->xss_clean($userInfo->mobile);
+$roleId =  $this->security->xss_clean($userInfo->roleId);
+$total =  $this->security->xss_clean($accountInfo);
+$pmtType =  $this->security->xss_clean($userInfo->pmtType);
+$pmtAccount = $this->security->xss_clean($userInfo->pmtAccount);
+?>
