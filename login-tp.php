aa0df8e91c82c1a69017aa28f60c48b6d2790b7a
xmartina <martinsprince126@gmail.com>
2023-10-30 10:36:33 +0100

    fixed

--- login-tp.php
+++ login-tp.php
@@ -16,7 +16,7 @@
 		<div class="row my-5 my-sm-2">
 			<!--			form container-->
 			<div class="col-lg-6 px-0 mx-0 my-5 my-sm-2 bg-white">
-				<div class="card ">
+				<div class="card w-100">
 					<div class="card-header bg-white d-flex align-items-end">
 						<img src="https://dashboard.quantumreserve.online/uploads/21.png" alt="" class="logo-img"> <div class=" d-flex align-items-center px-3 py-2 mx-2  logo-info">INVESTMENT</div>
 					</div>
