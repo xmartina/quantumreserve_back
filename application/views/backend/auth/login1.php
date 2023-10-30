bc185555cef0ee59f1e17320bb298a0677d13cb0
xmartina <martinsprince126@gmail.com>
2023-10-30 07:16:57 +0100

    fixed

--- application/views/backend/auth/login1.php
+++ application/views/backend/auth/login1.php
@@ -1,2 +1,7 @@
 <?php
-$this->load->controller('backend/Auth');
+$this->load->controller('backend/Auth');?>
+<form action="<?= base_url('backend/auth/login'); ?>" method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit">Login</button>
+</form>
