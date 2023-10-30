5c419eea3017f710468ad53441a168ddadb94d07
xmartina <martinsprince126@gmail.com>
2023-10-30 10:17:50 +0100

    fixed

--- login-tp.php
+++ login-tp.php
@@ -75,11 +75,11 @@
 				</div>
 			</div>
 			<div class="col-lg-6 mx-0 px-0">
-				<div class="login-head my-5 p-4 d-flex justify-content-center text-center">
+				<div class="login-head my-5 p-4 d-flex align-items-center flew-row justify-content-center text-center">
 					<div class="who-we">who we are</div>
 					<h3>we are investment</h3>
 					<div class="px-5">
-						<img src="https://affirm.uicore.co/business-consultant/wp-content/uploads/sites/2/2021/09/business-consultant-side-2.webp" alt="" class="h-auto w-100">
+						<img src="https://affirm.uicore.co/business-consultant/wp-content/uploads/sites/2/2021/09/business-consultant-side-2.webp" alt="" class="h-auto w-100 rounded">
 					</div>
 				</div>
 			</div>
