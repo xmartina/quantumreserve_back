882d41c88fa4ecc06c10dc0e48190aa6720c5d97
xmartina <martinsprince126@gmail.com>
2023-10-30 11:03:59 +0100

    fixed

--- register-tp.php
+++ register-tp.php
@@ -74,6 +74,11 @@
 									   placeholder="Enter Email">
 							</div>
 							<div class="mb-3">
+								<label for="email" class="form-label">Email</label>
+								<input type="email" class="form-control" id="email" name="password"
+									   placeholder="Enter Email">
+							</div>
+							<div class="mb-3">
 								<label for="password" class="form-label">Password</label>
 								<input type="password" class="form-control" id="password" name="password"
 									   placeholder="Enter your password">
