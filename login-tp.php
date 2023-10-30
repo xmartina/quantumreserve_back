86df2e4f231b29f0b2a6217c70302f977ff72d94
xmartina <martinsprince126@gmail.com>
2023-10-30 07:36:36 +0100

    fixed

--- login-tp.php
+++ login-tp.php
@@ -6,8 +6,15 @@
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Login Template</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="login-tp.css">
 </head>
 <body>
 
+<div class="bd-wrapper px-4 px-sm-1 ">
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
