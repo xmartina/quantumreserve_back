a212d2e23c5f303eb3c286e4a19577bbf265cfb5
xmartina <martinsprince126@gmail.com>
2023-10-30 11:10:25 +0100

    fixed

--- register-tp.php
+++ register-tp.php
@@ -81,39 +81,40 @@
 							</div>
 							<div class="mb-3">
 								<label for="email" class="form-label">Email</label>
-								<input type="tel" class="form-control" id="phoneit" aria-describedby="phone" name="password"
+								<input type="tel" class="form-control" id="phoneit" aria-describedby="phone"
+									   name="password"
 									   placeholder="Enter Email">
 								<label class="error" id="phone"></label>
 							</div>
+							<div class="mb-3">
+								<label for="password" class="form-label">Password</label>
+								<input type="password" class="form-control" id="password" name="password"
+									   placeholder="Enter your password">
+							</div>
+							<div class="mb-3">
+								<input type="checkbox" class="form-check-input" id="remember_me" name="remember_me">
+								<label for="remember_me" class="form-check-label">Remember me</label>
+							</div>
+							<button type="submit" class="btn mb-4 w-75 btn-primary">Login</button>
+							<div class="py-3"></div>
+						</form>
 					</div>
-					<div class="mb-3">
-						<label for="password" class="form-label">Password</label>
-						<input type="password" class="form-control" id="password" name="password"
-							   placeholder="Enter your password">
-					</div>
-					<div class="mb-3">
-						<input type="checkbox" class="form-check-input" id="remember_me" name="remember_me">
-						<label for="remember_me" class="form-check-label">Remember me</label>
-					</div>
-					<button type="submit" class="btn mb-4 w-75 btn-primary">Login</button>
-					<div class="py-3"></div>
-					</form>
 				</div>
 			</div>
-		</div>
-		<div class="col-lg-6 d-none d-lg-block d-xl-block bg-dark dd mx-0 px-0">
-			<div class="login-head my-5 p-4 d-flex align-items-center flew-column justify-content-center text-center">
-				<div class="who-we">who we are</div>
-				<h3 class="">we are investment</h3>
-				<div class="px-5 ac justify-self-end align-self-end">
-					<img
-						src="https://affirm.uicore.co/business-consultant/wp-content/uploads/sites/2/2021/09/business-consultant-side-2.webp"
-						alt="" class="h-auto w-100 rounded">
+			<div class="col-lg-6 d-none d-lg-block d-xl-block bg-dark dd mx-0 px-0">
+				<div
+					class="login-head my-5 p-4 d-flex align-items-center flew-column justify-content-center text-center">
+					<div class="who-we">who we are</div>
+					<h3 class="">we are investment</h3>
+					<div class="px-5 ac justify-self-end align-self-end">
+						<img
+							src="https://affirm.uicore.co/business-consultant/wp-content/uploads/sites/2/2021/09/business-consultant-side-2.webp"
+							alt="" class="h-auto w-100 rounded">
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
