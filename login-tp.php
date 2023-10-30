cca5d904944539ece32231bdea3e8c4de999499e
xmartina <martinsprince126@gmail.com>
2023-10-30 10:38:07 +0100

    fixed

--- login-tp.php
+++ login-tp.php
@@ -39,7 +39,7 @@
 										<span class="material-symbols-outlined">chevron_right</span>
 									</div>
 								</div>
-								<div class="col-2"></div>
+								<div class="col-2 d-sm-none d-md-block d-lg-block d-xl-block"></div>
 								<div class="col-5 px-2 ml-2 bg-light py-2 d-flex justify-content-between align-items-center">
 									<div class="mr-3"><span class="material-symbols-outlined">warning</span></div>
 									<div class="m-0 pb-1">
