2659eef9e555f2c3db591966ed3e6108d6eb8195
xmartina <martinsprince126@gmail.com>
2023-10-30 07:43:11 +0100

    fixed

--- login-tp.php
+++ login-tp.php
@@ -12,7 +12,38 @@
 <body>
 
 <div class="bd-wrapper px-4 px-sm-1 ">
-
+	<div class="container">
+		<div class="row">
+			<!--			form container-->
+			<div class="col-lg-6 bg-white">
+				<div class="card">
+					<div class="card-header">
+						<h3>Login</h3>
+					</div>
+					<div class="card-body">
+						<form action="/login" method="post">
+							<div class="mb-3">
+								<label for="username" class="form-label">Username</label>
+								<input type="text" class="form-control" id="username" name="username"
+									   placeholder="Enter your username">
+							</div>
+							<div class="mb-3">
+								<label for="password" class="form-label">Password</label>
+								<input type="password" class="form-control" id="password" name="password"
+									   placeholder="Enter your password">
+							</div>
+							<div class="mb-3">
+								<input type="checkbox" class="form-check-input" id="remember_me" name="remember_me">
+								<label for="remember_me" class="form-check-label">Remember me</label>
+							</div>
+							<button type="submit" class="btn btn-primary">Login</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6"></div>
+		</div>
+	</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
