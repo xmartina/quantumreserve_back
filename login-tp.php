4b5906305a06934abdef331f93e3223338199260
xmartina <martinsprince126@gmail.com>
2023-10-30 09:23:13 +0100

    fixed

--- login-tp.php
+++ login-tp.php
@@ -32,7 +32,9 @@
 							<div class="row">
 								<div class="col-6 bg-light py-2 d-flex justify-content-between align-items-center">
 									<div class="mr-3"><span class="material-symbols-outlined m-0 p-0">account_circle</span></div>
-									<a href="https://dashboard.quantumreserve.online/signup" class="text-dark m-0 p-0">Create Account</a>
+									<div class="m-0 p-0">
+										<a href="https://dashboard.quantumreserve.online/signup" class="text-dark m-0 p-0">Create Account</a>
+									</div>
 									<div class="m-0 p-0">
 										<span class="material-symbols-outlined">chevron_right</span>
 									</div>
